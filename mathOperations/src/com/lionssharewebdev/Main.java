00be61152960fb625108c6fc30db298f7163b7de
adrienne <adriennebing@gmail.com>
2017-07-17 14:09:40 -0400

    scanner and print to screen operations work

--- mathOperations/src/com/lionssharewebdev/Main.java
+++ mathOperations/src/com/lionssharewebdev/Main.java
@@ -1,17 +1,41 @@
 package com.lionssharewebdev;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-	    String input1;
-	    String input2;
-	    double operand1;
-	    double operand2;
 	    double sum;
 	    double diff;
-	    double division;
+	    double div;
 	    double multiply;
+	    int remainder;
+
+	    System.out.println("Please enter a number.");
+        Scanner scanner1 = new Scanner(System.in);
+        String input1  = scanner1.nextLine();
+		System.out.println("The first number is " + input1);
+
+        System.out.println("Please enter a second number.");
+        Scanner scanner2 = new Scanner(System.in);
+        String input2 = scanner2.nextLine();
+		System.out.println("The second number is " + input2);
+
+		double operand1 = Double.parseDouble(input1);
+		double operand2 = Double.parseDouble(input2);
+
+
 
     }
+
+
+
+    /*
+
+    public static void showResults(sum,diff,div,multiply,remainder){
+
+	}
+
+	*/
 }
